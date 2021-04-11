44406232d13542acbcf4a692a4bc9623f5ea215f
Felix Böhm <boehm.felix@gmx.net>
2021-04-11 18:25:12 +0200

    .

--- src/App.svelte
+++ src/App.svelte
@@ -1,71 +1,26 @@
 <script>
-	import { categories, categories_try, myCircles } from "./data/data.js";
-	// import radius_rescale from "./scaling_functions.svelte";
-	console.log(categories[0].neighbours[0].cat, myCircles[0]);
-
-    let count_max = function(category){
-        let count = 0;
-        let str = "_count";
-        for (let j in category){
-            if (j.search("_count") != -1){
-                count = count + category[j];			
-                }
-        }
-        return count
-	}
-
-	const radius_rescale = function(radius_max, count, category) {
-		return (count/count_max(category))*radius_max
-	}
-
-	const get_line_coord = function(circlenum, xy) {
-		if (circlenum != null){
-			console.log(circlenum)
-			if (xy === "x"){
-				return myCircles[circlenum][circlenum].x
-			} else {
-				return myCircles[circlenum][circlenum].y
-			}
-		}
-	}
-	const line_show = function(){
-		document.getElementById("lines").style.visibility = "visible";
-	}
-
-	const radius_max = 20;
+	import { each } from 'svelte/internal';
+import { ludocircles } from './data/data.js';
 </script>
-<svg height=500 width=500>
-{#each categories as category, i}
-	<circle cx={myCircles[i][i].x} cy={myCircles[i][i].y} r={radius_max} class = "outercircle" on:click={line_show()}/>
-	<circle cx={myCircles[i][i].x} cy={myCircles[i][i].y} r={radius_rescale(category.maincat_count, radius_max, category)} class = "innercircle"/>
-	{#each category.neighbours as neighbour}
-		<line id="lines" x1={myCircles[i][i].x} y1={myCircles[i][i].y} x2={get_line_coord(neighbour.cat, "x")} y2={get_line_coord(neighbour.cat, "y")} style="stroke-width: {neighbour.count};"/>
-	{/each}
-	<text x={myCircles[i][i].x} y={myCircles[i][i].y}>{category.maincat}</text>
-{/each}
-</svg>
 
-{#each categories as category, i}
-	<li>{category.maincat} - {category.maincat_count}- {i}</li>
-{/each}
+<svg height = 1000 width= 1000>
+	{#each ludocircles as circle, i}
+	<circle cy={circle.y} cx = {circle.x} r=40 class = "outercircle"/>
+	<text x={circle.x} y={circle.y} id="text">{i}</text>
+	{/each}
+</svg>
 
 <style>
 	svg {
-		background-color: seagreen;
+		background-color: green;
+	}
+	#text {
+		color: black;
+	}
+	circle {
+		background-color: yellow;
 	}
 	.outercircle {
-		fill: red;
-		cursor: pointer;
-	}
-	.innercircle {
-		fill: gray;
-		cursor: pointer;
-	}
-	line {
-		stroke: greenyellow;
-		opacity: 0;
-	}
-	line:hover {
-		opacity: 1;
+		fill: yellow;
 	}
 </style>
---
+++ src/reactive_points.svelte
@@ -0,0 +1,91 @@
+<script>
+	import { categories, categories_try, myCircles } from "./data/data.js";
+	// import radius_rescale from "./scaling_functions.svelte";
+	console.log(categories[0].neighbours[0].cat, myCircles[0]);
+
+    let count_max = function(category){
+        let count = 0;
+        let str = "_count";
+        for (let j in category){
+            if (j.search("_count") != -1){
+                count = count + category[j];			
+                }
+        }
+        return count
+	}
+
+	const radius_rescale = function(radius_max, count, category) {
+		return (count/count_max(category))*radius_max
+	}
+
+	const get_line_coord = function(circlenum, xy) {
+		if (circlenum != null){
+			console.log(circlenum)
+			if (xy === "x"){
+				return myCircles[circlenum][circlenum].x
+			} else {
+				return myCircles[circlenum][circlenum].y
+			}
+		}
+	}
+
+let lines = [];
+
+
+
+	function line_show2(){
+		lines.array.forEach(element => {
+			element.style.display = 'none'
+		});
+	}
+	// const line_show = function(){
+	// 	const lines = document.getElementsByTagName("lines");
+	// 	lines.forEach(e => e.style.display= 'none');
+	// }
+
+
+
+	const radius_max = 20;
+</script>
+<svg height=500 width=500>
+{#each categories as category, i}
+
+	<circle cx={myCircles[i][i].x} cy={myCircles[i][i].y} r={radius_max} class = "outercircle" id = {category.maincat} on:click={line_show2}/>
+	<circle cx={myCircles[i][i].x} cy={myCircles[i][i].y} r={radius_rescale(category.maincat_count, radius_max, category)} class = "innercircle"/>
+	<text x={myCircles[i][i].x} y={myCircles[i][i].y}>{category.maincat}</text>
+	{#each category.neighbours as neighbour, j}
+		<line class="lines" href={category.maincat} bind:this={lines[j]} x1={myCircles[i][i].x} y1={myCircles[i][i].y} x2={get_line_coord(neighbour.cat, "x")} y2={get_line_coord(neighbour.cat, "y")} style="stroke-width: {neighbour.count};"/>
+	{/each}
+{/each}
+</svg>
+<button on:click={line_show2}>Hier klicken</button>
+{#each categories as category, i}
+	<li>{category.maincat} - {category.maincat_count}- {i}</li>
+{/each}
+
+<style>
+	svg {
+		background-color: seagreen;
+	}
+	.outercircle {
+		fill: red;
+		cursor: pointer;
+	}
+	.innercircle {
+		fill: gray;
+		cursor: pointer;
+	}
+	line {
+		stroke: greenyellow;
+		opacity: 0.3;
+	}
+	#cat1:hover ~ [href="cat1"]{
+		opacity: 1;
+	}
+	#cat2:hover ~ [href="cat2"]{
+		opacity: 1;
+	}
+	#cat3:hover ~ [href="cat3"]{
+		opacity: 1;
+	}
+</style>
